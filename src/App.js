93668d535b2cb2c12aafe107394d34da50e2eb2a
BuildTools <arthurpisakhov1@gmail.com>
2019-09-04 19:13:52 -0400

    date, title, explanation

--- src/App.js
+++ src/App.js
@@ -3,22 +3,29 @@ import axios from 'axios';
 import "./App.css";
 
 function App() {
-  let today = new Date().toLocaleString()
   const [photo, setPhoto] = useState('');
+  const [title, setTitle] = useState('');
+  const [about, setAbout] = useState('');
+  const [date, setDate] = useState('');
+
   const fetchPhoto = () => 
     axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
       .then(photo => {
         console.log(photo);
         setPhoto(photo.data.url)
+        setTitle(photo.data.title)
+        setAbout(photo.data.explanation)
+        setDate(photo.data.date)
       });
 
   return (
     <div className="App">
       <h1>NASA Photo of the Day!</h1>
       <img className="nasa-photo" src={photo} alt=""/>
-      <h2>Today is: {today}</h2>
       <div>
-        <p></p>
+        <p>{title}</p>
+        <p>{date}</p>
+        <p>{about}</p>
       </div>
       <button onClick={e => fetchPhoto()}>Click Test</button>
     </div>
--- src/components/FetchPhoto.js
+++ src/components/FetchPhoto.js
@@ -2,13 +2,5 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 function photoFetch() {
-  const [photo, setPhoto] = useState('');
-  const fetchPhoto = () => 
-    fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
-      .then(res => res.json())
-      .then(photo => {
-        console.log(photo);
-        setPhoto(photo.url)
-      });
 
 }
