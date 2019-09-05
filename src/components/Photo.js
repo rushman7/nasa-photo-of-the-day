1ec876fdc5811a72f19f2b295fa3484379a4a006
BuildTools <arthurpisakhov1@gmail.com>
2019-09-04 20:40:17 -0400

    added react-date-picker

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,18 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Data from './Data';
+import DatePicker from 'react-date-picker';
 
 function Photo() {
   const [photo, setPhoto] = useState('');
   const [title, setTitle] = useState('');
   const [about, setAbout] = useState('');
   const [date, setDate] = useState('');
+  const [newDate, setNewDate] = useState(new Date());
 
   useEffect(() => {
     const fetchPhoto = () => 
-      axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2012-03-14')
+      axios.get(`https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2016-12-08`)
         .then(photo => {
-          console.log(photo);
           setPhoto(photo.data.url)
           setTitle(photo.data.title)
           setAbout(photo.data.explanation)
@@ -21,8 +22,17 @@ function Photo() {
       fetchPhoto()
   }, [])
 
+  const onChange = newDate => setNewDate(newDate)
+  console.log(newDate);
+
   return (
-    <div>
+    <div className="photo-div">
+      <DatePicker
+        onChange={onChange}
+        value={newDate}
+        format='yyyy-MM-dd'
+        maxDate={new Date()}
+      />
       <img className="nasa-photo" src={photo} alt=""/>
       <Data title={title} about={about} date={date}/>
     </div>
