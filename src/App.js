b75fb84de4a62f66eecdf95923bce3ca86ced6a7
BuildTools <arthurpisakhov1@gmail.com>
2019-09-05 14:14:42 -0400

    added reactstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Photo from './components/Photo';
-import "./App.css";
+// import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
